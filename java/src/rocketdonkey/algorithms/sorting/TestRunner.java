a094184ffe315c8227b2d417a5661c09fdd0535f
RocketDonkey <taylor@taylorbowland.com>
2014-11-21 08:48:06 -0800

    Fix runner instructions.

--- java/src/rocketdonkey/algorithms/sorting/TestRunner.java
+++ java/src/rocketdonkey/algorithms/sorting/TestRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.notification.Failure;
  * Test runner for Java sorting algorithms.
  * To run: To run this, go to the classes/
  *   1. Go to the classes/ directory.
- *   2. Run javac -d ../src/rocketdonkey/algorithms/sorting/*.java.
+ *   2. Run javac -d ./ ../src/rocketdonkey/algorithms/sorting/*.java.
  *   3. Run java rocketdonkey.algorithms.sorting.TestRunner.
  */
 public class TestRunner {
